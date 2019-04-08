ff81ea45d4b58764c476d76170daf628a3213e94
lam1101999 <nguyenvuduclam@gmail.com>
2019-04-08 08:40:01 +0700

    fix

--- PvZ/src/GameObject/GameObject.java
+++ PvZ/src/GameObject/GameObject.java
@@ -25,49 +25,38 @@ public abstract class GameObject {
     protected int y;
     protected int width;
 	protected int height;
-	
-	
-    public int getWidth() {
+    
+    public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getWidth() {
 		return width;
 	}
-
 	public void setWidth(int width) {
 		this.width = width;
 	}
-
 	public int getHeight() {
 		return height;
 	}
-
 	public void setHeight(int height) {
 		this.height = height;
 	}
-	
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-    
-    public abstract void tick();
+	public abstract void tick();
     public abstract void render(Graphics g);
     public abstract void remove();
     
     public Rectangle getBound() {
     	return new Rectangle(x,y,width,height);
     }
-
-    
 }
+
 
